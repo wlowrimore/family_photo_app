ed04048fa21d7e297a8c757e21c5b73902eb5f08
William Lowrimore <wlowrimore@gmail.com>
2022-09-04 14:01:31 -0500

    creates active storage photo upload

---
+++ app/models/pin.rb
@@ -0,0 +1,20 @@
+class Pin < ApplicationRecord
+    has_one_attached :image
+    has_many_attached :pictures
+    has_rich_text :body
+
+    def image_as_thumbnail
+        return unless image.content_type.in?(%w[image/jpeg image/png])
+        image.variant(resize_to_limit: [300, 300]).processed
+    end
+
+    def pictures_as_thumbnails
+        pictures.map do |picture|
+            picture.variant(resize_to_limit: [150, 150]).processed
+        end
+    end
+
+    def picture_as_thumbnail(pic)
+        pic.variant(resize_to_limit: [200, 200]).processed
+    end
+end
